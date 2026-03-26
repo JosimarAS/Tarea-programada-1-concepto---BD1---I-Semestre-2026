2e3d5579a0bf59e7809035a5ef4b04e9284f8a5b
Charlie <c.marin.1@estudiantec.cr>
2026-03-26 02:45:50 -0600

    Update to v1_database.sql
    Stored procedures para obtener lista de empleados, y para insertar empleado

--- v1_database.sql
+++ v1_database.sql
@@ -82,3 +82,160 @@ BEGIN
         , ('Carmen Sanchez', 480000.00) 
 END 
 GO
+
+-- ===================================================
+
+-- SP: Obtener lista de empleados
+
+IF EXISTS (
+    SELECT *
+    FROM sys.objects
+    WHERE (object_id = OBJECT_ID(N'dbo.sp_ObtenerEmpleados'))
+      AND (type = 'P')
+)
+BEGIN
+    DROP PROCEDURE dbo.sp_ObtenerEmpleados
+END
+GO
+
+CREATE PROCEDURE dbo.sp_ObtenerEmpleados
+    @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    BEGIN TRY
+
+        SELECT
+            emp.id
+            , emp.Nombre
+            , emp.Salario
+        FROM
+            dbo.Empleado AS emp
+        ORDER BY
+            emp.Nombre ASC
+
+        SET @outResultCode = 0
+
+    END TRY
+    BEGIN CATCH
+
+        INSERT INTO dbo.DBErrors
+        (
+            ErrorNumber
+            , ErrorSeverity
+            , ErrorState
+            , ErrorProcedure
+            , ErrorLine
+            , ErrorMessage
+        )
+        VALUES
+        (
+            ERROR_NUMBER()
+            , ERROR_SEVERITY()
+            , ERROR_STATE()
+            , ERROR_PROCEDURE()
+            , ERROR_LINE()
+            , ERROR_MESSAGE()
+        )
+
+        SET @outResultCode = 50001
+
+    END CATCH
+
+END
+GO
+    
+--==============================================
+    
+-- SP: Insertar un empleado
+
+    IF EXISTS (
+    SELECT *
+    FROM sys.objects
+    WHERE (object_id = OBJECT_ID(N'dbo.sp_InsertarEmpleado'))
+      AND (type = 'P')
+)
+BEGIN
+    DROP PROCEDURE dbo.sp_InsertarEmpleado
+END
+GO
+
+CREATE PROCEDURE dbo.sp_InsertarEmpleado
+    @inNombre      VARCHAR(128)
+    , @inSalario   MONEY
+    , @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- Pre-proceso: verificar si el nombre ya existe
+    DECLARE @nombreExiste INT
+
+    BEGIN TRY
+
+        SELECT
+            @nombreExiste = COUNT(emp.id)
+        FROM
+            dbo.Empleado AS emp
+        WHERE
+            (emp.Nombre = @inNombre)
+
+        IF (@nombreExiste > 0)
+        BEGIN
+            SET @outResultCode = 50002
+            RETURN
+        END
+
+        -- Transaccion al final del SP
+        BEGIN TRANSACTION
+
+            INSERT INTO dbo.Empleado
+            (
+                Nombre
+                , Salario
+            )
+            VALUES
+            (
+                @inNombre
+                , @inSalario
+            )
+
+        COMMIT TRANSACTION
+
+        SET @outResultCode = 0
+
+    END TRY
+    BEGIN CATCH
+
+        IF (@@TRANCOUNT > 0)
+        BEGIN
+            ROLLBACK TRANSACTION
+        END
+
+        INSERT INTO dbo.DBErrors
+        (
+            ErrorNumber
+            , ErrorSeverity
+            , ErrorState
+            , ErrorProcedure
+            , ErrorLine
+            , ErrorMessage
+        )
+        VALUES
+        (
+            ERROR_NUMBER()
+            , ERROR_SEVERITY()
+            , ERROR_STATE()
+            , ERROR_PROCEDURE()
+            , ERROR_LINE()
+            , ERROR_MESSAGE()
+        )
+
+        SET @outResultCode = 50001
+
+    END CATCH
+
+END
+GO
+
